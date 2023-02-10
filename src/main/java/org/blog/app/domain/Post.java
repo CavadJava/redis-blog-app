7d406b76c3c3c38cd10c4594d64d2ae1a278ed68
Javad Bagirov <javad.bagirov@unibank.az>
2023-02-10 20:21:24 +0400

    persistence configration for post domain

--- src/main/java/org/blog/app/domain/Post.java
+++ src/main/java/org/blog/app/domain/Post.java
@@ -5,13 +5,22 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(schema = "blog_app")
+@Entity
 public class Post{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
