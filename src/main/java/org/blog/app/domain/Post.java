f27be4a5ea65a8a0edd198495d1a53428c550e2a
Javad Bagirov <javad.bagirov@unibank.az>
2023-02-10 20:19:28 +0400

    Post domain added

---
+++ src/main/java/org/blog/app/domain/Post.java
@@ -0,0 +1,17 @@
+package org.blog.app.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post{
+
+    private Long id;
+    private String name;
+}
